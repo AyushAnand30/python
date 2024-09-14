8ccf2a6602708646234d7b639a8f8b54f94d3210
AyushAnand30 <Ayushshivam609@gmail.com>
2024-09-15 00:22:04 +0530

    new python commit added

---
+++ Ayush/Dictonireis/first/first.py
@@ -0,0 +1,8 @@
+dict1 = {}
+x = int(input("Enter the marks of the physics: "))
+y = int(input("Enter the marks of the chemistry: "))
+z = int(input("Enter the marks of the maths: "))
+dict1.update({"Phy ": x})
+dict1.update({"Chem " :y})
+dict1.update({"Math: " : z})
+print(dict1)
---
+++ Ayush/List/storing_in_list.py
@@ -0,0 +1,5 @@
+a = input("Enter your  favourite movie: ")
+b = input("Enter your  favourite movie: ")
+c = input("Enter your  favourite movie: ")
+list1 = [a, b, c]
+print(list1)
